19a0e03868a1c4e12de3f5d11058c094f4f142bc
lionleeee <lee97hun3@gmail.com>
2023-09-14 20:22:52 +0900

    SalesCalculator 프로그램

---
+++ first-console-app/Sales/SalesCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_console_app.Sales
+{
+	class SalesCalculator
+	{
+		static void Main(string[] args)
+		{
+			var sales = new SalesCounter("sales.csv");
+			var amountPerStore = sales.GetPerStoreSales();
+			foreach(var obj in amountPerStore)
+			{
+				Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+			}
+		}
+	}
+
+}
---
+++ first-console-app/Sales/SalesCounter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_console_app.Sales
+{
+	public class SalesCounter
+	{
+		private IEnumerable<Sale> _sales;
+
+		public SalesCounter(string filePath)
+		{
+			_sales = ReadSales(filePath);
+		}
+		private static IEnumerable<Sale> ReadSales(string filepath)
+		{
+			var sales = new List<Sale>();
+			var lines = File.ReadLines(filepath);
+			
+			foreach(var line in lines)
+			{
+				var items = line.Split(',');
+				var sale = new Sale
+				{
+					ShopName = items[0],
+					ProductCategory = items[1],
+					Amount = int.Parse(items[2])
+				};
+				sales.Add(sale);
+
+			}
+			return sales;
+		}
+
+		public IDictionary<string, int> GetPerStoreSales()
+		{
+			var dict = new Dictionary<string, int>();
+			foreach(var sale in _sales)
+			{
+				if (dict.ContainsKey(sale.ShopName)) dict[sale.ShopName] += sale.Amount;
+				else dict[sale.ShopName] = sale.Amount;
+			}
+			return dict;
+		}
+	}
+}
