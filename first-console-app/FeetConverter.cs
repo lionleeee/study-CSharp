3b307ac9996f0270725817bb5a8a956de60748d1
lionleeee <lee97hun3@gmail.com>
2023-09-14 19:49:29 +0900

    -

---
+++ first-console-app/FeetConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace first_console_app;
+
+
+public static class FeetConverter
+{
+    public static readonly double Ratio = 0.3048;
+
+    public static double FormMeter(double meter)
+    {
+        return meter / Ratio;
+    }
+
+    public static double ToMeter(double feet)
+    {
+        return feet * Ratio;
+    }
+    
+}
---
+++ first-console-app/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using first_console_app;
+
+
+PrintFeetToMeterList(1,10);
+PrintMeterToFeetList(1,10);
+
+static void PrintFeetToMeterList(int start, int stop)
+{
+    for (int feet = start; feet <= stop; feet++)
+    {
+        double meter = FeetConverter.ToMeter(feet);
+        Console.WriteLine("{0}  ft = {1:0.0000} m ", feet, meter);
+    }
+}
+
+static void PrintMeterToFeetList(int start, int stop)
+{
+    for (int  meter= start; meter <= stop; meter++)
+    {
+        double feet = FeetConverter.FormMeter(meter);
+        Console.WriteLine("{0}  ft = {1:0.0000} m ", meter, feet);
+    } 
+}
+
+
